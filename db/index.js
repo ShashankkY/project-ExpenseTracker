bb260435b6b894bd9e0551afab171fea94669b06
ShashankkY <shashankyogesh12@gmail.com>
2025-08-03 00:26:09 +0530

    final update

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_PASSWORD,
   {
     host: 'localhost',
     dialect: 'mysql',
